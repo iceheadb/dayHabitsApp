5271f37819e005a03c29140ce1ccb4f7ad9eb67c
iceheadb <victorlopeszb@gmail.com>
2023-01-31 20:18:18 -0300

    command modal instaled in front

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -20,10 +20,10 @@ export function SummaryTable() {
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
-                {weekDays.map(weekDay => {
+                {weekDays.map((weekDay, i) => {
                     return (
                         <div 
-                            //key={`${weekDay}-${i}`}
+                            key={`${weekDay}-${i}`}
                             className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
                         >
                             {weekDay}
